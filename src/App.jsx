e4fd62d7a4320946aa8e7c95f4100340c930c829
Satya Prakash <satyaprakash91180@gmail.com>
2024-02-18 15:09:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Navbaar from "./components/Navbar";
+import Main from "./components/Main";
+
+const App = () => {
+  return (
+    <div>
+      <Navbaar />
+      <Main />
+    </div>
+  );
+};
+
+export default App;
